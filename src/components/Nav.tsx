bab478058f4b80da694c573e8755b980c87e88f6
Jpeterson <peterson.jacob13@protonmail.com>
2025-04-13 16:27:37 -0500

    added navbar logic, candidate interface

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,19 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const location = useLocation().pathname;
   return (
-    <div>Nav</div>
+    <div>
+      <ul>
+        <li>
+          <Link to='/' className={location === '/' ? 'nav-link-active' : 'nav-link'}>Home</Link>
+        </li>
+        <li>
+          <Link to='SavedCandidates' className={location === 'SavedCandidates' ? 'nav-link-active' : 'nav-link'}>Potential Candidates</Link>
+        </li>
+      </ul>
+    </div>
   )
 };
 
